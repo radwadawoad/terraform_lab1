82f9cc85adeedabf82662c2578f212a9bca55b95
radwadawoad <50420973+radwadawoad@users.noreply.github.com>
2022-10-23 17:07:58 +0200

    Delete route.tf

--- terraform/route.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route" "r1" {
-  route_table_id              = aws_route_table.public_table.id
-}
-
-
-resource "aws_route" "r2" {
-  route_table_id              = aws_route_table.privat_table.id
-}
